abb2abc6cf1984382eb6cc0bd6ca76f59a135bcc
hanna-ediza <hannadaphne.ediza@gmail.com>
2022-03-10 16:48:51 +0100

    Add new functions

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+console.log("You are currently viewing Hanna's portfolio site");
+
+function menuToggle() {
+}
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+}
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+}
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
